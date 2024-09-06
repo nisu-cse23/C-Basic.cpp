83bca3b9476a11ab8f26bf6fb721a29e993f48eb
nisu-cse23 <160548482+nisu-cse23@users.noreply.github.com>
2024-09-06 23:44:55 +0600

    Add files via upload

---
+++ DataTypes.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int number=100;
+    char ch='A';
+    float data=93.33;
+    printf("Integer Number:%d\n",number);
+    printf("Character :%c\n",ch);
+    printf("Floating-data point:%.2f\n",data);
+    return 0;
+
+}
---
+++ Uni.cpp
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    printf("Most. Fatematuzzohura Nisu\nUniversity of chattogram");
+    return 0;
+}
