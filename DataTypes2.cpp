d568fb9ffd607ecbcbb151a01256dde6ebbd7b5a
nisu-cse23 <160548482+nisu-cse23@users.noreply.github.com>
2024-09-06 23:57:24 +0600

    Add files via upload

---
+++ DataTypes2.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    
+    char ch;
+    int number;
+
+
+    printf("Enter the character:");
+    scanf("%c",&ch);
+    printf("Given character is:%c\n",ch);
+    
+
+    printf("\n\nEnter the number:");
+    scanf("%d",&number);
+    printf("Given number:%d",number);
+
+    
+    return 0;
+}
