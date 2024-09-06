3631991913fc68c0ba4fbb44f2b9a9aa416eefad
nisu-cse23 <160548482+nisu-cse23@users.noreply.github.com>
2024-09-07 00:23:43 +0600

    Add files via upload

---
+++ AssIncDecOperator.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int x=5;
+    
+    x++;
+    printf("The value of x:%d\n",x);
+    x+=3;
+    printf("The value of x:%d\n",x);
+    x-=2;
+    printf("The value of x:%d\n",x);
+    x--;
+    printf("The value of x:%d\n",x);
+    return 0;
+}
